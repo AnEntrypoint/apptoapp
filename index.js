98d36b5077135a501274f7566f7ee6d77d89aae5
lanmower <almagestfraternite@gmail.com>
2023-09-25 10:31:05 +0000

    fixed json

--- index.js
+++ index.js
@@ -8,10 +8,11 @@ const tokens = require('gpt3-tokenizer').default
 const tokenizer = new tokens({ type: 'gpt3' });
 const beautify = require("js-beautify/js").js;
 const instarray = [...process.argv];
+
 instarray.shift();
 instarray.shift();
 const transformationInstruction = instarray.join(' ');
-const systemPrompt = `${transformationInstruction}. Don't include any explanations in your responses, don't include unmodified files in your responses, include all the modified or added files complete without comments. Only respond in this syntax: ^^filename^filecontents^^|^^filename^filecontents^^`;
+const systemPrompt = `${transformationInstruction}. Don't include any explanations in your responses, don't include unmodified files in your responses, include all the modified or added files complete without comments. Only respond in this syntax: #^filename&^filecontents#^filename&^filecontents#^`;
 var minify = require('html-minifier').minify;
 const htmlbeautify = require("js-beautify/js").html;
 if(!process.env.OPENAI_API_KEY) {
@@ -19,6 +20,7 @@ if(!process.env.OPENAI_API_KEY) {
     process.exit();
 }
 console.log(transformationInstruction)
+
 
 async function generateJsonData() {
     try {
@@ -70,6 +72,9 @@ async function generateJsonData() {
                                     comments: false,
                                 }
                             })).code;
+                        } else if (filePath.endsWith('.json')) {
+                            console.log('MINIFYING JSON', { filePath })
+                            result = JSON.stringify(eval(fileContent));
                         } else if (filePath.endsWith('.svelte')) {
                             const Terser = require('terser');
                             console.log('TERSERING SVELTE', { filePath })
@@ -112,7 +117,7 @@ async function generateJsonData() {
         }
         await readsrcdirectory('./');
         // Save the generated JSON data to a file
-        const generatedJsonData = Object.keys(jsonEntries).map(a => `^^${a}^${jsonEntries[a]}^^`).join('|'); // Pretty-print JSON
+        const generatedJsonData = Object.keys(jsonEntries).map(a => `#^${a}&^${jsonEntries[a]}`).join(''); // Pretty-print JSON
         let total = 1
         const message = `${generatedJsonData}`;
         total += tokenizer.encode(`${transformationInstruction} in the following application:\n\n${message}` ).bpe.length + tokenizer.encode(systemPrompt).bpe.length + 15
@@ -141,38 +146,49 @@ async function generateJsonData() {
         }
         const text = response.choices[0].message.content.trim();
         fs.writeFileSync('transformed.out', text)
-        function writeFilesFromStr(str) {
-            const files = str.split('^^|');
-            files.forEach(file => {
-                console.log({file})
-                const parts = file.split('^');
-                const filePath = parts[2];
-                const fileContent = parts[3];
-                writeFile(filePath, fileContent);
-            });
-          
-            function writeFile(filePath, content) {
-              const directory = path.dirname(filePath);
-              fs.mkdirSync(directory, { recursive: true });
-          
-              if(path.extname(filePath) == '.js' || path.extname(filePath) == '.jsx' || path.extname(filePath) == '.svelte') {
-                content = beautify(content, { indent_size: 2, space_in_empty_paren: true });
-              }
-          
-              if(path.extname(filePath) == '.html' || path.extname(filePath) == '.ejs') {
-                content = htmlbeautify(content, { indent_size: 2, preserve_newlines: true });
-              }
-          
-              fs.writeFileSync(filePath, content, 'utf8');
-            }
-        }
-        writeFilesFromStr(text)
-
-
+        return text;
     } catch (error) {
-        console.error(error)
+        console.trace(error)
         console.error('Error:', error.response.data);
     }
 }
+function writeFilesFromStr(str) {
+    const files = str.split('#^');
+    console.log({files})
+    files.shift()
+    files.forEach(file => {
+        console.log({file})
+        const parts = file.split('&^');
+        console.log({parts})
+        const filePath = parts[0];
+        const fileContent = parts[1];
+        writeFile(filePath, fileContent);
+    });
+  
+    function writeFile(filePath, content) {
+      const directory = path.dirname(filePath);
+      fs.mkdirSync(directory, { recursive: true });
+  
+      if(path.extname(filePath) == '.js' || path.extname(filePath) == '.jsx') {
+        content = beautify(content, { indent_size: 2, space_in_empty_paren: true });
+      }
+  
+      if(path.extname(filePath) == '.json') {
+        content = JSON.stringify(JSON.parse(content, null, 2);
+      }
+      if(path.extname(filePath) == '.html' || path.extname(filePath) == '.ejs' || path.extname(filePath) == '.svelte') {
+        content = htmlbeautify(content, { indent_size: 2, preserve_newlines: true });
+      }
+  
+      fs.writeFileSync(filePath, content, 'utf8');
+    }
+}
+if(instarray[0] === 'rewrite') {
+    console.log('rewrite')
+    text = fs.readFileSync('transformed.out')
+    writeFilesFromStr(text.toString())
+} else {
+    const text = generateJsonData()
+    writeFilesFromStr(text)
+}
 
-generateJsonData();
