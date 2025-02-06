ff10537196255a07a660e0a06a6dd606a28b1500
User <user@example.com>
2025-02-06 14:05:18 +0200

    refactor: simplify and modularize transformation library

--- index.js
+++ index.js
@@ -1,24 +1,3 @@
 #!/usr/bin/env node
-const fs = require('fs');
-const transformationLibrary = require('./transform');
 
-const instarray = [...process.argv];
-instarray.shift();
-instarray.shift();
-const transformationInstruction = instarray.join(' ');
-console.log("Current directory:", __dirname);
-async function main() {
-  try {
-    if (instarray[0] === 'rewrite') {
-      const text = fs.readFileSync('transformed.out');
-      transformationLibrary.writeFilesFromStr(text.toString());
-    } else {
-      const text = await transformationLibrary.generateJsonData(transformationInstruction);
-      transformationLibrary.writeFilesFromStr(text);
-    }
-  } catch (error) {
-    console.error('Error in CLI app:', error);
-  }
-}
-
-main();
+require('./src/index')
