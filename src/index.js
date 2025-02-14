2e3f46fb899fe209f8a9797fdf79753a9150cdb6
User <user@example.com>
2025-02-14 08:10:46 +0200

    prompt fix

--- src/index.js
+++ src/index.js
@@ -142,7 +142,7 @@ async function main(instruction, previousLogs) {
              
             + '\n// Change Tracking\n'
             + 'verify the previous changelog, and if the code changes in the changelog are not fully reflected in the codebase yet or have problems, edit the files accordingly\n'
-            + 'always respond with some text wrapped with <text></text> explaining all the file changes, explain the motivation for the changes and the edited files\n'
+            + 'always respond with some text wrapped with <text></text> explaining all the changes, explain the motivation for the changes and the cli commands used\n'
             
             + '\n// Output Formatting\n'
             + 'IMPORTANT: Only output file changes in xml format like this: <file path="path/to/edited/file.js">...</file> and cli commands in this schema <cli>command here</cli>\n'
