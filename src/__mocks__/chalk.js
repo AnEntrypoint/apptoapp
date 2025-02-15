f1b2f21974033e38a05c6cb0f274d296561274ef
Test User <test@example.com>
2025-02-15 20:44:50 +0200

    Changes from attempt 1

---
+++ src/__mocks__/chalk.js
@@ -0,0 +1,10 @@
+module.exports = {
+  blue: (str) => str,
+  green: (str) => str,
+  yellow: (str) => str,
+  red: (str) => str,
+  gray: (str) => str,
+  magenta: (str) => str,
+  cyan: (str) => str,
+  white: (str) => str
+}; 
--- src/test/logger.test.js
+++ src/test/logger.test.js
@@ -1,14 +1,4 @@
-// Mock chalk
-jest.mock('chalk', () => ({
-  blue: (str) => str,
-  green: (str) => str,
-  yellow: (str) => str,
-  red: (str) => str,
-  gray: (str) => str,
-  magenta: (str) => str,
-  cyan: (str) => str,
-  white: (str) => str
-}));
+jest.mock('chalk');
 
 describe('logger', () => {
   let consoleLogSpy;
