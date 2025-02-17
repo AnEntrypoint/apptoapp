9751d95985a79c84c8809a85a2ee3b60e8cd6632
Test User <test@example.com>
2025-02-17 20:29:19 +0200

    Changes from attempt 1

--- jest.config.js
+++ jest.config.js
@@ -1,24 +1,27 @@
 module.exports = {
-  testEnvironment: 'node',
   testTimeout: 20000,
+  maxWorkers: 1,
+  forceExit: true,
+  testEnvironment: 'node',
   setupFilesAfterEnv: ['./jest.setup.js'],
+  verbose: true,
+  // Automatically clear mock calls and instances between every test
+  clearMocks: true,
+  // Indicates whether the coverage information should be collected while executing the test
+  collectCoverage: false,
+  // The directory where Jest should output its coverage files
+  coverageDirectory: "coverage",
+  // A list of paths to directories that Jest should use to search for files in
+  roots: [
+    "<rootDir>/src"
+  ],
+  // The test pattern that Jest uses to detect test files
+  testMatch: [
+    "**/__tests__/**/*.[jt]s?(x)",
+    "**/?(*.)+(spec|test).[tj]s?(x)"
+  ],
+  // An array of regexp pattern strings that are matched against all test paths before executing
   testPathIgnorePatterns: [
-    '/node_modules/',
-    '/.cursor/',
-    '/.vscode/'
-  ],
-  modulePathIgnorePatterns: [
-    '/.cursor/',
-    '/.vscode/'
-  ],
-  watchPathIgnorePatterns: [
-    '/.cursor/',
-    '/.vscode/'
-  ],
-  reporters: [
-    'default',
-    'jest-junit'
-  ],
-  detectOpenHandles: true,
-  forceExit: true
+    "/node_modules/"
+  ]
 };
